084f454ebec4ed1f4a1c754fcc935cd3b2ff705a
Dima Pelyp <dpelip03@gmail.com>
2023-04-13 00:36:33 +0200

    menu animation change

--- js/menuItemAnim.js
+++ js/menuItemAnim.js
@@ -1,46 +1,139 @@
 let menuItems = document.querySelectorAll(".menu__list-item")
 
 menuItems.forEach((el)=>{
-    let pseudoElement = document.createElement("div")
-    pseudoElement.style.content = ""
-    pseudoElement.style.position = "absolute";
-    pseudoElement.style.width = "0%";
-    pseudoElement.style.height = "1px";
-    pseudoElement.style.backgroundColor = "#fff";
-    pseudoElement.style.bottom = "0";
-    pseudoElement.style.pointerEvents = "none"
-    
+    let pseudoElementTop = document.createElement("div")
+    pseudoElementTop.style.content = ""
+    pseudoElementTop.style.position = "absolute";
+    pseudoElementTop.style.width = "0%";
+    pseudoElementTop.style.height = "1px";
+    pseudoElementTop.style.backgroundColor = "#fff";
+    pseudoElementTop.style.top = "0";
+    pseudoElementTop.style.pointerEvents = "none"
+    el.appendChild(pseudoElementTop)
 
-    el.appendChild(pseudoElement)
+    let pseudoElementRight = document.createElement("div")
+    pseudoElementRight.style.content = ""
+    pseudoElementRight.style.position = "absolute";
+    pseudoElementRight.style.width = "1px";
+    pseudoElementRight.style.height = "0%";
+    pseudoElementRight.style.backgroundColor = "#fff";
+    pseudoElementRight.style.right = "0";
+    pseudoElementRight.style.pointerEvents = "none"
+    el.appendChild(pseudoElementRight)
 
-    const pseudoElementTimeline = gsap.timeline({
+    let pseudoElementLeft = document.createElement("div")
+    pseudoElementLeft.style.content = ""
+    pseudoElementLeft.style.position = "absolute";
+    pseudoElementLeft.style.width = "1px";
+    pseudoElementLeft.style.height = "0%";
+    pseudoElementLeft.style.backgroundColor = "#fff";
+    pseudoElementLeft.style.left = "0";
+    pseudoElementLeft.style.pointerEvents = "none"
+    el.appendChild(pseudoElementLeft)
+
+    let pseudoElementBottom = document.createElement("div")
+    pseudoElementBottom.style.content = ""
+    pseudoElementBottom.style.position = "absolute";
+    pseudoElementBottom.style.width = "0%";
+    pseudoElementBottom.style.height = "1px";
+    pseudoElementBottom.style.backgroundColor = "#fff";
+    pseudoElementBottom.style.bottom = "0";
+    pseudoElementBottom.style.pointerEvents = "none"
+    el.appendChild(pseudoElementBottom)
+
+    const pseudoElementTopTimeline = gsap.timeline({
         autoKill: true
     })
-    
-    el.addEventListener("mouseout", (event)=>{
+    const pseudoElementRightTimeline = gsap.timeline({
+        autoKill: true
+    })
+    const pseudoElementLeftTimeline = gsap.timeline({
+        autoKill: true
+    })
+    const pseudoElementBottomTimeline = gsap.timeline({
+        autoKill: true
+    })
+
+    el.querySelector(".menu__link").addEventListener("mouseout", (event)=>{
         if (event.target.tagName === 'DIV') {
             return;
         }
-        pseudoElementTimeline.add(gsap.to(el.querySelector("div"), {
+
+        pseudoElementTopTimeline.add(gsap.to(pseudoElementTop, {
             width: "0%",
-            right: 0,
             duration: .3,
-            onComplete: function(){
-                gsap.set(el.querySelector("div"), {clearProps: "right"})
+            onStart: function(){
+                gsap.set(pseudoElementTop, {clearProps: "right"})
+                gsap.set(pseudoElementTop, {left: 0})
+            }
+        }))
+        
+        pseudoElementRightTimeline.add(gsap.to(pseudoElementRight, {
+            height: "0%",
+            duration: .3,
+            onStart: function(){
+                gsap.set(pseudoElementRight, {clearProps: "bottom"})
+                gsap.set(pseudoElementRight, {top: 0})
+            }
+        }))
+
+        pseudoElementLeftTimeline.add(gsap.to(pseudoElementLeft, {
+            height: "0%",
+            duration: .3,
+            onStart: function(){
+                gsap.set(pseudoElementLeft, {clearProps: "top"})
+                gsap.set(pseudoElementLeft, {bottom: 0})
+            }
+        }))
+
+        pseudoElementBottomTimeline.add(gsap.to(pseudoElementBottom, {
+            width: "0%",
+            duration: .3,
+            onStart: function(){
+                gsap.set(pseudoElementBottom, {clearProps: "left"})
+                gsap.set(pseudoElementBottom, {right: 0})
             }
         }))
     })
 
-    el.addEventListener("mouseover", (event)=>{
+    el.querySelector(".menu__link").addEventListener("mouseover", (event)=>{
         if (event.target.tagName === 'DIV') {
             return;
         }
-        pseudoElementTimeline.add(gsap.to(el.querySelector("div"), {
+
+        pseudoElementTopTimeline.add(gsap.to(pseudoElementTop, {
             width: "100%",
-            left: 0, 
+            duration: .3,
+            onStart: function(){
+                gsap.set(pseudoElementTop, {clearProps: "left"})
+                gsap.set(pseudoElementTop, {right: 0})
+            }
+        }))
+
+        pseudoElementRightTimeline.add(gsap.to(pseudoElementRight, {
+            height: "100%",
+            duration: .3,
+            onStart: function(){
+                gsap.set(pseudoElementRight, {clearProps: "top"})
+                gsap.set(pseudoElementRight, {bottom: 0})
+            }
+        }))
+
+        pseudoElementLeftTimeline.add(gsap.to(pseudoElementLeft, {
+            height: "100%",
+            duration: .3,
+            onStart: function(){
+                gsap.set(pseudoElementLeft, {clearProps: "bottom"})
+                gsap.set(pseudoElementLeft, {top: 0})
+            }
+        }))
+
+        pseudoElementBottomTimeline.add(gsap.to(pseudoElementBottom, {
+            width: "100%",
             duration: .3, 
-            onComplete: function(){
-                gsap.set(el.querySelector("div"), {clearProps: "left"})
+            onStart: function(){
+                gsap.set(pseudoElementBottom, {clearProps: "right"})
+                gsap.set(pseudoElementBottom, {left: 0})
             }
         }))
     })
